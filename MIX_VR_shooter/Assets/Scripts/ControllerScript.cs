78bb505f6538ba4d0ad13caa1d1f63845a40c18e
David Weis <davidmichaelweis@gmail.com>
2017-04-13 03:12:37 +0200

    Add MIX_VR_shooter

---
+++ MIX_VR_shooter/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerScript : MonoBehaviour
+{
+
+   public GameObject Bullet;
+   public GameObject TeleporterBeam;
+
+   private SteamVR_TrackedObject trackedObject;
+   private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+   private float _timeOfLastShot;
+
+   // Use this for initialization
+   void Start()
+   {
+      TeleporterBeam.SetActive(false);
+      trackedObject = GetComponent<SteamVR_TrackedObject>();
+      _timeOfLastShot = Time.time;
+   }
+
+   // Update is called once per frame
+   void FixedUpdate()
+   {
+      if (Time.time - _timeOfLastShot > 0.150f && controller.GetHairTrigger())
+      {
+         _timeOfLastShot = Time.time;
+         print("Triggered");
+         GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+         newBullet.GetComponent<Rigidbody>().AddForce(Quaternion.AngleAxis(-35, transform.right) * transform.up * -20, ForceMode.VelocityChange);
+         Destroy(newBullet, 6);
+      }
+   }
+
+   void Update()
+   {
+      if (controller.GetPressDown(EVRButtonId.k_EButton_Grip))
+      {
+         TeleporterBeam.SetActive(true);
+      }
+      else if (controller.GetPressUp(EVRButtonId.k_EButton_Grip))
+      {
+         TeleporterBeam.SetActive(false);
+      }
+   }
+}
---
+++ MIX_VR_shooter/Assets/Scripts/DomeScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomeScript : MonoBehaviour
+{
+
+   public Transform Anchor;
+   public Material HitMaterial;
+   private Material _originalMaterial;
+   private float _lastDetect = 0f;
+   private bool _Engaged = false;
+   private const float CollisionTimeout = 0.1f;
+
+   // Use this for initialization
+   void Start ()
+	{
+	   _originalMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if (Time.time - _lastDetect > CollisionTimeout)
+	   {
+	      // not hit
+	      if (_Engaged)
+	      {
+	         _Engaged = false;
+	         GetComponent<Renderer>().material = _originalMaterial;
+	      }
+	   }
+	   else
+	   {
+	      // is engaged
+	      if (!_Engaged)
+	      {
+	         _Engaged = true;
+	         GetComponent<Renderer>().material = HitMaterial;
+	      }
+	   }
+	}
+
+   void OnTriggerStay(Collider other)
+   {
+      if (other.gameObject.name == "TeleporterBeam")
+      {
+         _lastDetect = Time.time;
+      }
+   }
+}
