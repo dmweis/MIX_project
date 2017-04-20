3270cc3e468e8ee867c439516af9d7e7abfa544d
Kristofer Carta <kriscarta@gmail.com>
2017-04-21 00:45:50 +0200

    Implement marker update remote notification behavior

--- CamerTracker/CameraTracker.Chessboard/App.xaml.cs
+++ CamerTracker/CameraTracker.Chessboard/App.xaml.cs
@@ -1,17 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Text;
 using System.Windows;
+using CameraTracker.Camera;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
 
 namespace CameraTracker.Chessboard
 {
-   /// <summary>
-   /// Interaction logic for App.xaml
-   /// </summary>
-   public partial class App : Application
-   {
-   }
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private TrackingService _tracker;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // For now we're just using the server locally, so we can hard-code this hostname
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare("marker_update", "fanout");
+
+            _tracker = new TrackingService(5, 5);
+
+            Observable
+                .FromEventPattern<MarkerChangeEventArgs>(_tracker, "MarkerChanged")
+                .Subscribe(evt =>
+                {
+                    var marker = new
+                    {
+                        Type = "update",
+                        Id = evt.EventArgs.MarkerId,
+                        Column = evt.EventArgs.X,
+                        Row = evt.EventArgs.Y
+                    };
+
+                    var message = JsonConvert.SerializeObject(marker);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    _channel.BasicPublish("marker_update", "", null, body);
+                });
+
+            Observable
+                .FromEventPattern<MarketDisappearedEventArgs>(_tracker, "MarkerDisappeared")
+                .Subscribe(evt =>
+                {
+                    var marker = new
+                    {
+                        Type = "remove",
+                        Id = evt.EventArgs.MarkerId
+                    };
+
+                    var message = JsonConvert.SerializeObject(marker);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    _channel.BasicPublish("marker_update", "", null, body);
+                });
+
+            MainWindow mainWindow = new MainWindow(_tracker);
+            mainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _tracker.Dispose();
+            _channel.Dispose();
+            _connection.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+
 }
