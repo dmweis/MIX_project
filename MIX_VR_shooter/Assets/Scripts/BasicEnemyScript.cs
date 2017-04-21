6d9ab32b5b1725a14887f7a578b8be921d9cb511
Kristofer Carta <kriscarta@gmail.com>
2017-04-21 16:45:32 +0200

    Add simple board implementation in MIX_VR_shooter

---
+++ MIX_VR_shooter/Assets/Scripts/BasicEnemyScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyScript : MonoBehaviour {
+
+    public GameObject NetworkManager;
+    public GameObject Board;
+    private NetworkHandler _handler;
+
+	// Use this for initialization
+	void Start () {
+        _handler = NetworkManager.GetComponent<NetworkHandler>();
+        if (_handler != null)
+        {
+            _handler.Register("marker_update", OnMove);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_handler == null)
+        {
+            _handler = NetworkManager.GetComponent<NetworkHandler>();
+            if (_handler != null)
+            {
+                _handler.Register("marker_update", OnMove);
+            }
+        }
+
+	}
+
+    private void OnMove(string message)
+    {
+        MarkerUpdate marker = JsonUtility.FromJson<MarkerUpdate>(message);
+        print("Moving to " + marker.Column + " " + marker.Row);
+        Vector3 newPosition = Board.GetComponent<Board>().GetAnchor(marker.Column, marker.Row);
+        transform.position = newPosition;
+    }
+}
+
+[Serializable]
+class MarkerUpdate
+{
+    public string Type;
+    public int Id;
+    public int Column;
+    public int Row;
+}
+
---
+++ MIX_VR_shooter/Assets/Scripts/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public GameObject BottomRight;
+    public int RowCount;
+    public int ColumnCount;
+
+    public Vector3 GetAnchor(int column, int row)
+    {
+        float xPos = Map(column, 0, ColumnCount, transform.position.x, BottomRight.transform.position.x);
+        float yPos = Map(row, 0, RowCount, transform.position.y, BottomRight.transform.position.y);
+        return new Vector3(xPos, yPos);
+    }
+
+    private static float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
