1cd014c53e6c744dae7e273213c645cf0a9f9770
Kristofer Carta <kriscarta@gmail.com>
2017-04-21 16:04:17 +0200

    Rewrite NetworkHandler to use new RabbitMQ setup and concurrent queue

--- MIX_VR_shooter/Assets/Scripts/NetworkHandler.cs
+++ MIX_VR_shooter/Assets/Scripts/NetworkHandler.cs
@@ -1,94 +1,62 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using RabbitMQ.Client.Framing;
 using UnityEngine;
+using System.Collections.Concurrent;
 
 public class NetworkHandler : MonoBehaviour
 {
+    private ConnectionFactory _connectionfactory;
+    private IConnection _connection;
+    private IModel _channel;
+    private readonly ConcurrentQueue<BasicDeliverEventArgs> _messageQueue = new ConcurrentQueue<BasicDeliverEventArgs>();
+    private readonly Dictionary<string, List<Action<string>>> _subscribers = new Dictionary<string, List<Action<string>>>();
 
-   private ConnectionFactory _connectionfactory;
-   private IConnection _connection;
-   private IModel _channel;
-   private readonly Queue<BasicDeliverEventArgs> _messageQueue = new Queue<BasicDeliverEventArgs>();
-   private readonly Dictionary<string, List<Action<RabbitMqMessage>>> _subscribers = new Dictionary<string, List<Action<RabbitMqMessage>>>();
+    // Use this for initialization
+    void Start()
+    {
+        _connectionfactory = new ConnectionFactory() { HostName = "localhost" };
+        _connection = _connectionfactory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
 
-   private readonly object _lock = new object();
+    public void Register(string exchangeName, Action<string> callback)
+    {
+        if (!_subscribers.ContainsKey(exchangeName))
+        {
+            _channel.ExchangeDeclare(exchangeName, "fanout");
+            _channel.QueueBind(_channel.QueueDeclare().QueueName, exchangeName, "");
+            new EventingBasicConsumer(_channel).Received += (sender, args) => _messageQueue.Enqueue(args);
+            _subscribers[exchangeName] = new List<Action<string>>();
+        }
+        _subscribers[exchangeName].Add(callback);
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-	   _connectionfactory = new ConnectionFactory() {HostName = "localhost"};
-	   _connection = _connectionfactory.CreateConnection();
-	   _channel = _connection.CreateModel();
-      Subscribe();
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-   private void Subscribe()
-   {
-      string exchangeName = "MIX_game";
-      string routingKey = "*.unity";
-      _channel.ExchangeDeclare(exchangeName, "topic");
-      string queueName = _channel.QueueDeclare().QueueName;
-      _channel.QueueBind(queueName, exchangeName, routingKey);
-      var consumer = new EventingBasicConsumer(_channel);
-      consumer.Received += (sender, args) =>
-      {
-         lock (_lock)
-         {
-            _messageQueue.Enqueue(args);
-         }
-      };
-   }
-
-   public void Register(string id, Action<RabbitMqMessage> callback)
-   {
-      if (_subscribers.ContainsKey(id))
-      {
-         _subscribers.Add(id, new List<Action<RabbitMqMessage>>());
-      }
-      _subscribers[id].Add(callback);
-   }
-
-	// Update is called once per frame
-	void Update () {
-	   lock (_lock)
-	   {
-	      foreach (BasicDeliverEventArgs messageArgs in _messageQueue)
-	      {
-	         string data = Encoding.UTF8.GetString(messageArgs.Body);
-	         RabbitMqMessage message = JsonUtility.FromJson<RabbitMqMessage>(data);
-	         if (_subscribers.ContainsKey(message.Key))
-	         {
-	            foreach (Action<RabbitMqMessage> callback in _subscribers[message.Key])
-	            {
-	               callback?.Invoke(message);
-	            }
-	         }
-	      }
-	   }
-	}
-}
-
-[Serializable]
-public class RabbitMqMessage
-{
-   public string Key;
-   public string Message;
-   private object _cache;
-
-   public T GetObject<T>()
-   {
-      if (_cache is T)
-      {
-         return (T) _cache;
-      }
-      T instance = JsonUtility.FromJson<T>(Message);
-      _cache = instance;
-      return instance;
-   }
+        while (_messageQueue.Count > 0)
+        {
+            BasicDeliverEventArgs messageArgs;
+            bool isDequeueSuccessful = _messageQueue.TryDequeue(out messageArgs);
+            if (isDequeueSuccessful)
+            {
+                string exchangeName = messageArgs.Exchange;
+                string jsonMessage = Encoding.UTF8.GetString(messageArgs.Body);
+                if (_subscribers.ContainsKey(exchangeName))
+                {
+                    foreach (Action<string> callback in _subscribers[exchangeName])
+                    {
+                        if (callback != null)
+                        {
+                            callback.Invoke(jsonMessage);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
