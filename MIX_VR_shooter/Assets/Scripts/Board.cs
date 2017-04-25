9017fa23553029ea52d695686a2637bef2705284
David Weis <davidmichaelweis@gmail.com>
2017-04-25 08:49:00 +0200

    Update board to move enemies

--- MIX_VR_shooter/Assets/Scripts/Board.cs
+++ MIX_VR_shooter/Assets/Scripts/Board.cs
@@ -1,22 +1,69 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
+public class Board : MonoBehaviour
+{
 
-    public GameObject BottomRight;
-    public int RowCount;
-    public int ColumnCount;
+   public GameObject BottomRight;
+   public int RowCount;
+   public int ColumnCount;
+   public GameObject EnemyPrefab;
+   public GameObject NetworkManager;
 
-    public Vector3 GetAnchor(int column, int row)
-    {
-        float xPos = Map(column, 0, ColumnCount, transform.position.x, BottomRight.transform.position.x);
-        float yPos = Map(row, 0, RowCount, transform.position.y, BottomRight.transform.position.y);
-        return new Vector3(xPos, yPos);
-    }
+   private Dictionary<int, EnemyBehaviour> _enemies;
+   private NetworkHandler _handler;
 
-    private static float Map(float value, float inMin, float inMax, float outMin, float outMax)
-    {
-        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-    }
+   void Start()
+   {
+      _enemies = new Dictionary<int, EnemyBehaviour>();
+      _handler = NetworkManager.GetComponent<NetworkHandler>();
+      if (_handler != null)
+      {
+         _handler.Register("marker_update", OnMove);
+         print("Registered for network updates");
+      }
+      else
+      {
+         print("failed to register on network manager");
+      }
+   }
+
+   private void OnMove(string message)
+   {
+      MarkerUpdate marker = JsonUtility.FromJson<MarkerUpdate>(message);
+      print("Moving " + marker.Id + " to " + marker.Row + " " + marker.Column);
+      Vector3 newPosition = GetAnchor(marker.Column, marker.Row);
+      if (!_enemies.ContainsKey(marker.Id))
+      {
+         GameObject newEnemy = Instantiate(EnemyPrefab, newPosition, Quaternion.identity);
+         _enemies.Add(marker.Id, newEnemy.GetComponent<EnemyBehaviour>());
+      }
+      else
+      {
+         _enemies[marker.Id].Move(newPosition);
+      }
+   }
+
+   private Vector3 GetAnchor(int column, int row)
+   {
+      float xPos = Map(column, 0, ColumnCount, transform.position.x, BottomRight.transform.position.x);
+      float yPos = Map(row, 0, RowCount, transform.position.y, BottomRight.transform.position.y);
+      return new Vector3(xPos, yPos);
+   }
+
+   private static float Map(float value, float inMin, float inMax, float outMin, float outMax)
+   {
+      return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+   }
+}
+
+[Serializable]
+class MarkerUpdate
+{
+   public string Type;
+   public int Id;
+   public int Column;
+   public int Row;
 }
